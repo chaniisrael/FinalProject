fa04957c0336517d177a1e5015012d012e927e50
chani <chanais@edu.hac.ac.il>
2022-06-07 21:25:06 +0300

    21

--- public/js/register.js
+++ public/js/register.js
@@ -1,8 +1,9 @@
 
 document.addEventListener('DOMContentLoaded', function () {
     debugger;
-     document.getElementById("formPassword").addEventListener("onsubmit", password);
-     document.getElementById("my23").addEventListener("onsubmit", login);
+     document.getElementById("submitPassword").addEventListener("click", password);
+
+    document.getElementById("my23").addEventListener("onsubmit", login);
      document.querySelector("#nextRegister").addEventListener("click", login2);
 
 
@@ -10,22 +11,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function login(event) {
     debugger;
-    // console.log('111');
-    // event.preventDefault();
-    // let password1 = cryptr.encrypt("nk")
-    // console.log(password1);
-    //  password1 = cryptr.decrypt(password1)
-    // console.log(password1)
+
     let userName = document.getElementById('userName').value;
-     let password = document.getElementById('password').value;
-     //let password=  document.getElementById('password').value=window.btoa(document.getElementById('password').value);
+     let password=  document.getElementById('password').value=window.btoa(document.getElementById('password').value);
 
-    console.log(password);
-    // password=  document.getElementById('password').value=window.atob(document.getElementById('password').value);
-    // console.log(password);
-
-    console.log("kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk")
-    //console.log(mail);
     fetch('/users/findUserName',{
         method:'post',
         headers: {
@@ -84,33 +73,32 @@ function login2(e) {
     });
 }
 //--------------------------------------------------------
-function password(event)
+function password(e)
 {
     debugger;
-    console.log("מלממממממממממממממממממממממממממממממממממממממממממ")
-    // e.preventDefault();
-    let password=  document.getElementById('password').value=window.btoa(document.getElementById('password').value);
-    let password2=  document.getElementById('password2').value=window.btoa(document.getElementById('password2').value);
+    console.log(e)
+     document.getElementById('password').value=window.btoa(document.getElementById('password').value);
+     document.getElementById('password2').value=window.btoa(document.getElementById('password2').value);
 
-    fetch('/register/save', {
-        method: 'post',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({password:password}),
-
-
-    }).then(function (res) {
-        res.json().then(function (data) {
-        //     console.log('su' + data);
-        //     if (data === 'not exist') {
-        //
-        //         document.getElementById("my22").submit();
-        //     } else
-        //         document.getElementById('error2').innerHTML = data;
-         })
-    }).catch(function (err) {
-        // need to display error message!
-        console.log('Fetch Error :', err);
-    });
+    // fetch('/register/save', {
+    //     method: 'post',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify({password:password}),
+    //
+    //
+    // }).then(function (res) {
+    //     res.json().then(function (data) {
+    //     //     console.log('su' + data);
+    //     //     if (data === 'not exist') {
+    //     //
+    //     //         document.getElementById("my22").submit();
+    //     //     } else
+    //     //         document.getElementById('error2').innerHTML = data;
+    //      })
+    // }).catch(function (err) {
+    //     // need to display error message!
+    //     console.log('Fetch Error :', err);
+    // });
 }
